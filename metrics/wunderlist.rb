251f74e6b54b1e60ab5b442bbf75f4e92d1b738e
Matt Horn <mathphreak@gmail.com>
2016-09-30 16:23:43 -0500

    Make Productivity load metrics dynamically

---
+++ metrics/wunderlist.rb
@@ -0,0 +1,59 @@
+require 'wunderlist'
+require 'yaml'
+require 'time'
+require 'active_support/core_ext/numeric/time'
+
+OGWunderlist = Wunderlist
+
+module Metrics
+  # Check if Wunderlist reports no upcoming tasks due.
+  class Wunderlist
+    def initialize(settings)
+      @cache = {}
+      @wl = OGWunderlist::API.new(
+        access_token: settings['access-token'],
+        client_id: settings['client-id']
+      )
+    end
+
+    def blocks
+      root = @wl.get 'api/v1/root'
+      id = root['id']
+      unless cache_revision(id) == root['revision'] &&
+             Time.now < @cache[id][:expires]
+        update_cache(root)
+      end
+      cached_problems
+    end
+
+    private
+
+    def cache_revision(id)
+      @cache[id].andand[:revision]
+    end
+
+    def task_if_problematic(list, task)
+      return if task.due_date.nil?
+      due_date = Time.parse(task.due_date)
+      "[#{list.title}] #{task.title}" if due_date < 1.5.days.from_now
+    end
+
+    def update_cache_for(list)
+      return if cache_revision(list.id) == list.revision
+      result = list.tasks.map do |task|
+        task_if_problematic list, task
+      end.compact
+      @cache[list.id] = { revision: list.revision, data: result }
+    end
+
+    def update_cache(root)
+      id = root['id']
+      @wl.lists.each { |list| update_cache_for list }
+      @cache[id] = { revision: root['revision'], expires: 30.minutes.from_now }
+    end
+
+    def cached_problems
+      @cache.values.map { |list| list[:data] } .flatten.compact
+    end
+  end
+end
--- productivity.rb
+++ productivity.rb
@@ -1,61 +1,19 @@
 # Detect productivity
 module Productivity
-  require 'wunderlist'
-  require 'yaml'
-  require 'time'
-  require 'active_support/core_ext/numeric/time'
-
-  @cache = {}
-
-  private_class_method def self.cache_revision(id)
-    @cache[id].andand[:revision]
+  metric_names = Dir.chdir('metrics') do
+    Dir['*'].map { |e| File.basename(e, '.rb') }
   end
 
-  private_class_method def self.task_if_problematic(list, task)
-    return if task.due_date.nil?
-    due_date = Time.parse(task.due_date)
-    three_half_days_from_now = 1.5.days.from_now
-    "[#{list.title}] #{task.title}" if due_date < three_half_days_from_now
-  end
+  metric_names.each { |m| require_relative "metrics/#{m}.rb" }
 
-  private_class_method def self.update_cache_for(list)
-    unless cache_revision(list.id) == list.revision
-      result = list.tasks.map do |task|
-        task_if_problematic list, task
-      end.compact
-      @cache[list.id] = { revision: list.revision, data: result }
-    end
-  end
+  settings = YAML.load_file('settings.yml')
 
-  private_class_method def self.update_cache(wl, root)
-    id = root['id']
-    wl.lists.each { |list| update_cache_for list }
-    @cache[id] = { revision: root['revision'], expires: 1.hour.from_now }
-  end
-
-  private_class_method def self.cached_problems
-    @cache.values.map { |list| list[:data] } .flatten.compact
-  end
-
-  private_class_method def self.problems(wl)
-    root = wl.get 'api/v1/root'
-    id = root['id']
-    unless cache_revision(id) == root['revision'] &&
-           Time.now < @cache[id][:expires]
-      update_cache(wl, root)
-    end
-    cached_problems
+  @metrics = Metrics.constants.map do |c|
+    "Metrics::#{c}".constantize.new settings[c.to_s]
   end
 
   # Returns a list of pending tasks for the next day and a half
   def self.blocks
-    secrets = YAML.load_file('secrets.yml')
-
-    wl = Wunderlist::API.new(
-      access_token: secrets['access-token'],
-      client_id: secrets['client-id']
-    )
-
-    problems wl
+    @metrics.map(&:blocks).flatten
   end
 end
